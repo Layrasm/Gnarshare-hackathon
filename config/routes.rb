08c69ab0ad9599c9341360c3161e00e691ce5bae
Layne Rasmussen <layrasm@gmail.com>
2020-10-10 16:06:04 -0600

    static controller added

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   namespace :api do 
     resources :posts do
       resources :comments
+
+      
     end
   end
+  get '*other', to: 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
